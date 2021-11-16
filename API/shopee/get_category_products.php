d678be8e7029ea29eaaeca6c0098b9931e32bcd4
Thái Phương Nam <thaiphuongnam1071@gmail.com>
2021-11-16 12:36:17 +0700

    Update get_category_products.php

--- API/shopee/get_category_products.php
+++ API/shopee/get_category_products.php
@@ -3,7 +3,7 @@
     $shop_categoryids = isset($_POST['categoryId']) ? $_POST['categoryId'] : '';
     $order = isset($_POST['order']) ? $_POST['order'] : 'asc';
     $newest = '0'; // <= 100
-    $limit = '40'; // <=100
+    $limit = '100'; // <=100
     $sortby = isset($_POST['sortby']) ? $_POST['sortby'] : 'pop'; //
     $url='https://shopee.vn/api/v4/search/search_items?by='.$sortby.'&limit='.$limit.'&match_id='.$shopid.'&newest='.$newest.'&order='.$order.'&page_type=shop&scenario=PAGE_OTHERS&shop_categoryids='.$shop_categoryids.'&version=2'; // tạo biến url cần lấy
     // $url='https://shopee.vn/api/v4/search/search_items?by=pop&limit=40&match_id=406356404&newest=0&order=desc&page_type=shop&scenario=PAGE_OTHERS&shop_categoryids='.$shop_categoryids.'&version=2'; // tạo biến url cần lấy
